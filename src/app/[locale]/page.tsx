b835c15d491fce2f6107f28f7314409790e2cf8d
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-06-25 18:58:33 +0700

    feat: Add basic content in about page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import FeaturesList from "@/features/features-list";
 import GetStarted from "@/features/get-started";
 import Hero from "@/features/hero";
 
-export default function Home() {
+export default function home() {
   return (
     <>
       <Hero />
