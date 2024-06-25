4d632040160f80380c483dab946754b984fdd4db
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-06-25 20:54:33 +0700

    update: Hero.tsx translantion en

--- src/features/get-started.tsx
+++ src/features/get-started.tsx
@@ -4,9 +4,7 @@ import { Button } from "@/components/ui/button";
 const GetStarted = () => {
   return (
     <Section className="flex flex-col items-center text-center py-12 px-4">
-      <Button className="mb-6 text-lg px-8 py-8 font-bold rounded-lg shadow-lg">
-        Get Started
-      </Button>
+      <Button className="mb-6 text-lg px-8 py-6 font-bold">Get Started</Button>
       <div className="max-w-4xl">
         <p className="text-lg md:text-xl font-light mb-6">
           To get started with My EZ Start Boilerplate, clone the repository and
--- src/features/hero.tsx
+++ src/features/hero.tsx
@@ -1,19 +1,23 @@
 import Section from "@/components/layout/section";
 import { AuroraBackground } from "@/components/ui/aurora-background";
+import { Button } from "@/components/ui/button";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
 
 const Hero = () => {
+  const t = useTranslations("App.Home.Hero");
   return (
     <AuroraBackground className="text-foreground w-screen h-fit pt-28 pb-10 md:h-screen">
-      <Section className="max-w-4xl text-justify md:text-center px-6">
-        <h1 className="text-3xl md:text-7xl font-bold text-center mb-5">
-          Welcome to My EZ Start Boilerplate
+      <Section className="max-w-4xl text-justify md:text-center px-6 gap-10">
+        <h1 className="text-3xl md:text-7xl font-bold text-center">
+          {t("title")}
         </h1>
         <p className="font-extralight text-base md:text-2xl">
-          My EZ Start Boilerplate is a comprehensive and flexible starting point
-          for your web development projects. Built with modern technologies and
-          best practices, it helps you kickstart your development process with a
-          robust foundation.
+          {t("description")}
         </p>
+        <Button asChild className="mb-6 text-lg px-8 py-6 font-bold">
+          <Link href="/get-started">{t("cta")}</Link>
+        </Button>
       </Section>
     </AuroraBackground>
   );
