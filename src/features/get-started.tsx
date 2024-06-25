ee4702422515ac2ec31e38f43bd1fd27e4112469
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-06-25 22:26:22 +0700

    feat: Add GetStarted section with translations and improve FeaturesList to handle SVG components
    - Implement GetStarted component with button and translated content.
    - Update FeaturesList component to dynamically load and render SVG components from translations.
    - Improve layout and responsiveness for better user experience.
    - Update translation JSON file to include GetStarted section and enhance FeaturesList descriptions.

--- src/features/get-started.tsx
+++ src/features/get-started.tsx
@@ -1,22 +1,22 @@
 import Section from "@/components/layout/section";
 import { Button } from "@/components/ui/button";
+import { useTranslations } from "next-intl";
 
 const GetStarted = () => {
+  const t = useTranslations("App.Home.GetStarted");
+  const content = t.raw("content") as string[];
+
   return (
-    <Section className="flex flex-col items-center text-center py-12 px-4">
-      <Button className="mb-6 text-lg px-8 py-6 font-bold">Get Started</Button>
+    <Section className="flex flex-col items-center text-justify md:text-center py-12 px-4">
+      <Button className="mb-6 text-lg px-8 py-6 font-bold">
+        {t("buttonText")}
+      </Button>
       <div className="max-w-4xl">
-        <p className="text-lg md:text-xl font-light mb-6">
-          To get started with My EZ Start Boilerplate, clone the repository and
-          follow the setup instructions in the README file. Customize the
-          configuration files to suit your project needs, and start building
-          your application with ease.
-        </p>
-        <p className="text-lg md:text-xl font-light">
-          This boilerplate is designed to be flexible and extensible, allowing
-          you to add features and modify existing ones to fit your specific
-          requirements.
-        </p>
+        {content.map((paragraph: string, index: number) => (
+          <p className="text-lg md:text-xl font-light mb-6" key={index}>
+            {paragraph}
+          </p>
+        ))}
       </div>
     </Section>
   );
