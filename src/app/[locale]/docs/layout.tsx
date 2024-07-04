96e85830d2c8f99cff7b9d321f59ecb6cfcc6760
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-07-05 01:04:27 +0700

    test

--- src/app/[locale]/docs/layout.tsx
+++ src/app/[locale]/docs/layout.tsx
@@ -2,7 +2,7 @@ import Nav from "@/components/layout/nav";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mt-[88px] flex w-full h-full">
+    <div className="mt-20 flex w-full h-full">
       <Nav
         t="SideNav"
         render="links"
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,21 +2,32 @@
 import Burger from "@/components/header/burger";
 import Nav from "@/components/header/nav";
 import UserMenu from "@/components/header/user-menu";
+import useOnScroll from "@/hooks/useOnScroll";
 import { cn } from "@/lib/utils";
 import LocaleSwitcher from "@/providers/language/locale-switcher";
 import { ThemeSwitcher } from "@/providers/theme/theme-switcher";
 import { useSession } from "next-auth/react";
+import { useLocale } from "next-intl";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 import UserAuthLinks from "./user-auth-links";
 import UserNav from "./user-nav";
 
 export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+  const locale = useLocale();
+  const scrollY = useOnScroll();
   const { data: session } = useSession();
   const user = session?.user;
   return (
-    <header className="px-3 py-6 ">
+    <header
+      className={cn("px-3 py-6 lg:py-5 z-10 fixed w-full", {
+        "bg-background": isOpen || scrollY > 0,
+        "p-2": scrollY > 0,
+      })}
+    >
       <div className="mx-auto max-w-screen-lg">
         <div className="flex flex-wrap items-center justify-between">
           <Link href="/">
