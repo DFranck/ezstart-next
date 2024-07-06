4ec7f02361ea0cc7c5b2509fdea43187351e0ea8
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-07-06 20:04:51 +0700

    fix: empty page.tsx add minimum content

---
+++ src/app/[locale]/(user)/settings/page.tsx
@@ -0,0 +1,12 @@
+import ComingSoon from "@/components/layout/coming-soon.";
+import Section from "@/components/layout/section";
+
+const page = () => {
+  return (
+    <Section>
+      <ComingSoon />
+    </Section>
+  );
+};
+
+export default page;
