f4975eefe7152beab952b8f411634a897baf58e9
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-07-10 15:38:23 +0700

    add coming soon component

---
+++ src/components/layout/coming-soon.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+type ComingSoonProps = {
+  message?: string;
+};
+
+const ComingSoon: React.FC<ComingSoonProps> = ({ message }) => {
+  return (
+    <div className="flex flex-col items-center justify-center h-full text-center p-8 text-muted-foreground">
+      <h2>Coming Soon</h2>
+      <p className=" mt-2">
+        {message || "This page is under construction. Please check back later!"}
+      </p>
+      <div className="mt-4">
+        <svg
+          className="w-12 h-12 text-gray-300 animate-spin"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8v8H4z"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoon;
---
+++ src/components/sign-in-button.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { LogIn } from "lucide-react";
+import { useLocale, useTranslations } from "next-intl";
+import Link from "next/link";
+const SignInLink = () => {
+  const locale = useLocale();
+  const t = useTranslations("Components.SignInLink");
+  return (
+    <Button asChild className="">
+      <Link href={`/${locale}/sign-in`}>
+        <LogIn className="mr-2 w-4" />
+        <span className="sr-only">{t("description")}</span>
+        {t("title")}
+      </Link>
+    </Button>
+  );
+};
+
+export default SignInLink;
