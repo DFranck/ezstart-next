305f62c7e8451efd595e0074cfb63aaabef594b2
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-07-15 17:36:20 +0700

    add external social Img

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -28,17 +28,40 @@ export default async function RootLayout({
   params: { locale: string };
 }>) {
   const messages = await getMessages();
+  const socialImage = "https://i.ibb.co/tsk3MLp/opengraph-image-2-1.png";
   return (
     <html lang={locale} suppressHydrationWarning={true} className="h-full">
       <head>
-        <meta
-          name="twitter:image"
-          content={"https://i.ibb.co/tsk3MLp/opengraph-image-2-1.png"}
-        />
-        <meta
-          name="og:image"
-          content={"https://i.ibb.co/tsk3MLp/opengraph-image-2-1.png"}
-        />
+        <head>
+          <meta
+            property="og:title"
+            content="EzStart - The Ultimate Boilerplate for Modern Web Development"
+          />
+          <meta
+            property="og:description"
+            content="Jumpstart your web development projects with EzStart - The Ultimate Boilerplate for Modern Web Development, a comprehensive and flexible boilerplate. Built with modern technologies and best practices, EzStart provides a robust foundation for creating high-performance, scalable web applications."
+          />
+          <meta property="og:image" content={socialImage} />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+          <meta property="og:image:alt" content="EzStart" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://ez-start.vercel.app/en" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta
+            name="twitter:title"
+            content="EzStart - The Ultimate Boilerplate for Modern Web Development"
+          />
+          <meta
+            name="twitter:description"
+            content="Jumpstart your web development projects with EzStart - The Ultimate Boilerplate for Modern Web Development, a comprehensive and flexible boilerplate. Built with modern technologies and best practices, EzStart provides a robust foundation for creating high-performance, scalable web applications."
+          />
+          <meta name="twitter:image" content={socialImage} />
+          <meta name="twitter:image:width" content="1200" />
+          <meta name="twitter:image:height" content="630" />
+          <meta name="twitter:image:alt" content="EzStart" />
+        </head>
       </head>
       <body
         className={cn(inter.className, "min-h-screen flex flex-col h-full")}
