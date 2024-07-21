cd0c15cf93b30cb2c393415c092434d34f017573
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-07-22 04:43:55 +0700

    feat(enhancee utility functions for class name merging): enhance utility functions for class merging

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
+// src/lib/utils.ts
+
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+/**
+ * Utility function to merge class names using clsx and tailwind-merge.
+ *
+ * @param inputs - An array of class values to be merged
+ * @returns A single string of merged class names
+ */
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
